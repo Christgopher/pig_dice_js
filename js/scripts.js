1efae65bc19762434bd1476e1c7bcd57786dc3eb
Steve & Jim Patt <student@epicodus.com>
2015-06-03 10:56:17 -0700

    add function to keep track of overall score

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,21 @@ function Player(name, score) {
 }
 
 $(function(){
+
+  var currentPlayer = new Player("Joe") // temporary default player
+
+  $("#current-player-score").text(currentPlayer.score);
+
   var tally = 0;
   var dieRoll = 0;
   $("#roll-die").click(function(event) {
-    event.preventDefault();
     dieRoll = Math.floor(Math.random()*(6)+1);
     if (dieRoll !== 1) {
       tally += dieRoll;
     } else {
       tally = 0;
     }
-    // debugger;
+
     $("#tally").text(tally);
     $("#dieRoll").text(dieRoll);
 
@@ -29,8 +33,15 @@ $(function(){
 
 
 
-  $("form#current-tally").submit(function(event) {
-    event.preventDefault();
+  $("#hold").click(function(event) {
+    event.preventDefault;
+    currentPlayer.addToScore(tally);
+    $("#current-player-score").text(currentPlayer.score);
+    tally = 0;
+    dieRoll = 0;
+    $("#tally").text(tally);
+    $("#dieRoll").text(dieRoll);
+
 
   });
 
